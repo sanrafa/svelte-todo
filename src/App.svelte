fb71c3d65f5b72070d5854f2bbc0dd6103432df3
Rafa Brown <shane.rafael@zohomail.com>
2022-01-05 23:05:48 -0700

    Add Welcome comp w/ name input

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script>
-	
+	import Welcome from "./Welcome.svelte";
+	import {name} from './stores';
 </script>
 
 <main>
-	
+	<h1>Whatcha Gotta Do?</h1>
+	{#if $name}
+	<h2>Hello {$name}!</h2>
+	{/if}
+	<Welcome />	
 </main>
 
 <style>
-	
+	main {
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
---
+++ src/Welcome.svelte
@@ -0,0 +1,16 @@
+<script>
+import {name} from './stores';
+let username;
+
+function handleSubmit () {
+    name.set(username);
+}
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <p>
+        First, what's your name?
+    </p>
+    <input type="text" bind:value="{username}"/>
+    <button type="submit">ENTER</button>
+</form>
