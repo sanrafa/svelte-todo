74b5727581012da5128772117ecfb9b3bb6652f3
Rafa Brown <shane.rafael@zohomail.com>
2022-01-06 13:25:35 -0700

    Setup test environment

---
+++ src/test/app.spec.js
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+
+import { render, fireEvent } from "@testing-library/svelte";
+
+describe("TODO app", () => {
+  it("works", () => {
+    expect(1).toEqual(1);
+  });
+});
