5f2d6f86b124cadb3f16d44a382b5e6d81efc545
Rafa Brown <shane.rafael@zohomail.com>
2022-01-07 14:11:43 -0700

    Add icons to SingleTodo component

--- src/SingleTodo.svelte
+++ src/SingleTodo.svelte
@@ -1,4 +1,6 @@
 <script>
+  import MdDelete from "svelte-icons/md/MdDelete.svelte";
+  import MdEdit from "svelte-icons/md/MdEdit.svelte";
   import { todos } from "./stores";
   export let id;
   export let task;
@@ -12,12 +14,24 @@
 <li>
   <input type="checkbox" bind:checked={completed} />
   {task}
-  <button type="button">EDIT</button>
-  <button type="button" on:click={handleDelete}>DELETE</button>
+  <button type="button" class="icon edit"><MdEdit /></button>
+  <button type="button" class="icon delete" on:click={handleDelete}
+    ><MdDelete /></button
+  >
 </li>
 
 <style>
   li {
     list-style-type: none;
+  }
+  .icon {
+    width: 32px;
+    height: 32px;
+  }
+  .delete {
+    color: darkred;
+  }
+  .edit {
+    color: blue;
   }
 </style>
