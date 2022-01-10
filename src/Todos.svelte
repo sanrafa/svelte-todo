e0ffe1e2b0ac112e0080681096aa9d5019d0af24
Rafa Brown <shane.rafael@zohomail.com>
2022-01-09 23:54:37 -0700

    Add drag/drop functionality via svelte-dnd-action

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -1,14 +1,23 @@
 <script>
+  import { dndzone } from "svelte-dnd-action";
   import { todos, toEdit } from "./stores";
   import SingleTodo from "./SingleTodo.svelte";
   import TodoForm from "./TodoForm.svelte";
 
   $: localStorage.setItem("todos", JSON.stringify($todos));
+
+  function handleSort(event) {
+    $todos = event.detail.items;
+  }
 </script>
 
 <section>
   <TodoForm text={Object.keys($toEdit) ? $toEdit.text : ""} />
-  <ul>
+  <ul
+    use:dndzone={{ items: $todos }}
+    on:consider={handleSort}
+    on:finalize={handleSort}
+  >
     {#each $todos as todo (todo.id)}
       <SingleTodo {todo} />
     {/each}
